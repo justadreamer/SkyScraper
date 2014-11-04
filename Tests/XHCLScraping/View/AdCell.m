90a08b68fb122806845ff9ce76c35483550cdb39
Eugene Dorfman <eugene.dorfman@gmail.com>
2014-11-04 22:42:29 +0200

    AdCell - added multithreading test

--- Tests/XHCLScraping/View/AdCell.m
+++ Tests/XHCLScraping/View/AdCell.m
@@ -10,14 +10,22 @@
 #import "AdData.h"
 #import "UIKit+AFNetworking.h"
 
+#import <XHTransformation/XHAll.h>
+#import "AFHTTPRequestOperation.h"
+
 @interface AdCell ()
 @property (nonatomic,strong) IBOutlet UILabel *title;
 @property (nonatomic,strong) IBOutlet UILabel *subtitle;
 @property (nonatomic,strong) IBOutlet UIImageView *thumbnail;
 @property (nonatomic,strong) IBOutlet NSLayoutConstraint *imageWidthConstraint;
+
+@property (nonatomic,strong) AFHTTPRequestOperation *operation;
 @end
 
 @implementation AdCell
+- (void) dealloc {
+    [self.operation cancel];
+}
 
 - (void)awakeFromNib {
     // Initialization code
@@ -54,6 +62,36 @@
         self.imageWidthConstraint.constant = 72.0;
         [self.thumbnail setImageWithURL:self.adData.thumbnailURL placeholderImage:nil];
     }
+    
+//this is called here jsut for testing multithreading - i.e. simulatenous usage of the same XHTransformation
+    [self loadDetailsForAdData];
+}
 
+- (void) loadDetailsForAdData {
+    [self.operation cancel];
+
+    XHTransformationHTMLResponseSerializer *serializer = [XHTransformationHTMLResponseSerializer serializerWithXHTransformation:[self.class detailTransformation] params:nil modelAdapter:nil];
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:self.adData.URL];
+    self.operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+    self.operation.responseSerializer = serializer;
+
+    [self.operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"responseObject=%@",responseObject);
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"%@",error);
+    }];
+
+    [self.operation start];
+
+}
+
++ (XHTransformation *) detailTransformation {
+    static XHTransformation *transformation = nil;
+    if (!transformation) {
+        NSURL *xslURL = [[NSBundle mainBundle] URLForResource:@"addetail" withExtension:@"xsl"];
+        transformation = [[XHTransformation alloc] initWithXSLTURL:xslURL];
+    }
+    return transformation;
 }
 @end
