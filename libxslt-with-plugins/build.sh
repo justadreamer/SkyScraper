e11ea39861e260b23596decfac91c6e49b6c8fe0
Eugene Dorfman <eugene.dorfman@gmail.com>
2016-01-09 14:46:34 +0100

    libxsl-with-plugins can be built for watchos

--- libxslt-with-plugins/build.sh
+++ libxslt-with-plugins/build.sh
@@ -1,5 +1,13 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 xcodebuild -sdk iphoneos
-xcodebuild -sdk iphonesimulator
-lipo -create build/Release-iphoneos/libxslt-with-plugins.a build/Release-iphonesimulator/libxslt-with-plugins.a -output libxslt-with-plugins.a
+xcodebuild -sdk watchos
+xcodebuild -sdk watchsimulator -arch i386
+xcodebuild -sdk iphonesimulator -arch x86_64
+
+lipo -create \
+build/Release-watchos/libxslt-with-plugins.a \
+build/Release-watchsimulator/libxslt-with-plugins.a \
+build/Release-iphoneos/libxslt-with-plugins.a \
+build/Release-iphonesimulator/libxslt-with-plugins.a \
+-output libxslt-with-plugins.a
